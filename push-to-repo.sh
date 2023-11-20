19de22c4955e8093bb2b1b06284babc1e405190d
Muhammad Mutasim Billah <130433248+mbahnaf@users.noreply.github.com>
2023-11-20 16:40:12 +0600

    Update push-to-repo.sh

--- push-to-repo.sh
+++ push-to-repo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cat /opt/my_password.txt | docker login --username mbahnaf --password-stdin
-docker build -t mbahnaf/my-mini-project .
-docker push mbahnaf/my-mini-project
+sudo docker build -t mbahnaf/my-mini-project .
+sudo docker push mbahnaf/my-mini-project

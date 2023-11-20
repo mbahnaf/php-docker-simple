efe36bd6330ef5027a8820a812924e131dc9d117
Muhammad Mutasim Billah <130433248+mbahnaf@users.noreply.github.com>
2023-11-20 17:21:44 +0600

    Update push-to-repo.sh

--- push-to-repo.sh
+++ push-to-repo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cat /opt/my_password.txt | docker login --username mbahnaf --password-stdin
-sudo docker build -t mbahnaf/my-mini-project -f /var/lib/jenkins/workspace/deploy-docker/Dockerfile
+sudo docker build -t mbahnaf/my-mini-project .
 sudo docker push mbahnaf/my-mini-project
